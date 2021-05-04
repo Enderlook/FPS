c36e8e4cd1b8f14393b10559a1f834cb7dc49db6
Marcos Biagi <marcos.biagi@gmail.com>
2021-05-04 16:17:51 -0300

    Third person character implemented & simple level

---
+++ Source/FPS/TPS_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TPS_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API ATPS_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
