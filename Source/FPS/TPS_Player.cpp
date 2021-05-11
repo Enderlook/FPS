4c936bfd01b4acd551d5b77dedf73bddc66450e0
Marcos Biagi <marcos.biagi@gmail.com>
2021-05-11 00:32:57 -0300

    enemy model replaced and animation re-targeted

--- Source/FPS/TPS_Player.cpp
+++ Source/FPS/TPS_Player.cpp
@@ -63,7 +63,8 @@ void ATPS_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATPS_Player::RestartGame);
+	PlayerInputComponent->BindAction("Restart", IE_Pressed, this, &ATPS_Player::RestartGame);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATPS_Player::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ATPS_Player::MoveForward);
